dbd909e981636299ba9d19e575410756298e608d
shidani0 <dima.waruga2003@gmail.com>
2023-12-01 23:30:20 +0300

    Добавьте файлы проекта.

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LAB6
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Продукция в магазине",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Lab6", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/Lab6Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LAB6.Controllers
+{
+    public class Lab6Controller : Controller
+    {
+        // GET: Lab6
+        public ActionResult Index()
+        {
+            List<string> tovar = GettovarList();
+            return View(tovar);
+        }
+
+        public ActionResult SecondView()
+        {
+            return View(GettovarList().OrderBy(x => x).ToList());
+        }
+
+        public ActionResult ThirdView()
+        {
+            return View(GettovarList().OrderBy(x => x).ToList());
+        }
+        public List<string> GettovarList()
+        {
+            List<string> tovar = new List<string>();
+            tovar.Add("Футболка");
+            tovar.Add("Джинсы");
+            tovar.Add("Шорты");
+            tovar.Add("Носки");
+            tovar.Add("Юбка");
+            tovar.Add("Кепка");
+            return tovar;
+        }
+    }
+
+   
+}
